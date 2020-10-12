34ec30a028e370966cb62cfdc6604187559c4790
Lucas Alves Costa <luc.kasnix@gmail.com>
2020-10-12 11:40:41 -0300

    Add recipe page

---
+++ pages/[recipe].tsx
@@ -0,0 +1,63 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import { jsonLdScriptProps } from 'react-schemaorg'
+import { Recipe } from 'schema-dts'
+import { GetServerSideProps } from 'next'
+
+export default function RecipeTemplate({ recipe }) {
+  return (
+    <div>
+      <Head>
+        <script
+          {...jsonLdScriptProps<Recipe>({
+            '@context': 'https://schema.org',
+            '@type': 'Recipe',
+            name: recipe.name,
+            image: recipe.image,
+            datePublished: recipe.datePublished,
+            description: recipe.description,
+            keywords: recipe.keywords,
+            recipeIngredient: recipe.ingredients,
+            recipeInstructions: recipe.instructions.map((instruction: string) => ({
+              '@type': 'HowToStep',
+              text: instruction
+            }))
+          })}
+        />
+      </Head>
+      <main>
+        <Link href="/">
+          <a>Go to home</a>
+        </Link>
+        <h1>{recipe.name}</h1>
+      </main>
+    </div>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  return {
+    props: {
+      recipe: {
+        slug: 'non-alcoholic-pina-colada',
+        name: 'Non-alcoholic Pina Colada',
+        image: [
+          'https://img.cybercook.com.br/receitas/312/pina-colada-5.jpeg'
+        ],
+        datePublished: '2018-03-10',
+        description: 'This non-alcoholic pina colada is my favorite!',
+        keywords: 'non-alcoholic',
+        ingredients: [
+          '2 cups of pineapple juice',
+          '5/8 cup cream of coconut',
+          'ice'
+        ],
+        instructions: [
+          'Blend 2 cups of pineapple juice and 5/8 cup cream of coconut until smooth.',
+          'Fill a glass with ice.',
+          'Pour the pineapple juice and coconut mixture over ice.'
+        ]
+      }
+    }
+  }
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,24 @@
-import { GetServerSideProps } from 'next'
 import Head from 'next/head'
-import { jsonLdScriptProps } from 'react-schemaorg'
-import { Recipe } from 'schema-dts'
+import Link from 'next/link'
+import { GetServerSideProps } from 'next'
 
-export default function Home({ recipe }) {
+export default function Home({ recipes }) {
   return (
     <div>
       <Head>
-        <script
-          {...jsonLdScriptProps<Recipe>({
-            '@context': 'https://schema.org',
-            '@type': 'Recipe',
-            name: recipe.name,
-            image: recipe.image,
-            datePublished: recipe.datePublished,
-            description: recipe.description,
-            keywords: recipe.keywords,
-            recipeIngredient: recipe.ingredients,
-            recipeInstructions: recipe.instructions.map((instruction: string) => ({
-              '@type': 'HowToStep',
-              text: instruction
-            }))
-          })}
-        />
+        <title>Find the best recipes ever</title>
       </Head>
       <main>
-        <h1>Receitas</h1>
+        <h1>Select a recipe</h1>
+        <ul>
+          {recipes.map(({ slug, name }: { slug: string, name: string }) => (
+            <li key={slug}>
+              <Link href="/[recipe]" as={`/${slug}`}>
+                <a>{name}</a>
+              </Link>
+            </li>
+          ))}
+        </ul>
       </main>
     </div>
   )
@@ -34,25 +27,12 @@ export default function Home({ recipe }) {
 export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
-      recipe: {
-        name: 'Non-alcoholic Pina Colada',
-        image: [
-          'https://img.cybercook.com.br/receitas/312/pina-colada-5.jpeg'
-        ],
-        datePublished: '2018-03-10',
-        description: 'This non-alcoholic pina colada is my favorite!',
-        keywords: 'non-alcoholic',
-        ingredients: [
-          '2 cups of pineapple juice',
-          '5/8 cup cream of coconut',
-          'ice'
-        ],
-        instructions: [
-          'Blend 2 cups of pineapple juice and 5/8 cup cream of coconut until smooth.',
-          'Fill a glass with ice.',
-          'Pour the pineapple juice and coconut mixture over ice.'
-        ]
-      }
+      recipes: [
+        {
+          slug: 'non-alcoholic-pina-colada',
+          name: 'Non-alcoholic Pina Colada'
+        }
+      ]
     }
   }
 }
