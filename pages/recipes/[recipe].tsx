c816a931777b50f5d5637c26aabe715285cede16
Lucas Alves Costa <luc.kasnix@gmail.com>
2020-10-27 15:19:58 -0300

    Add Open Graph tags

--- pages/recipes/[recipe].tsx
+++ pages/recipes/[recipe].tsx
@@ -4,18 +4,25 @@ import { jsonLdScriptProps } from 'react-schemaorg'
 import { Recipe } from 'schema-dts'
 import { GetServerSideProps } from 'next'
 
-export default function RecipeTemplate({ recipe }) {
+export default function RecipePage({ recipe }) {
   return (
     <>
       <Head>
         <title>{recipe.name} | Awesome Recipes</title>
         <meta name='description' content={recipe.description}/>
+        <meta property='og:url' content={`http://localhost:3000/recipes/${recipe.slug}`}/>
+        <meta property='og:site_name' content='Awesome Recipes'/>
+        <meta property='og:title' content={recipe.name}/>
+        <meta property='og:description' content={recipe.description}/>
+        <meta property='og:type' content='website'/>
+        <meta property='og:image' content={recipe.image.url}/>
+        <meta property='og:locale' content={recipe.locale.replace('-', '_')}/>
         <script
           {...jsonLdScriptProps<Recipe>({
             '@context': 'https://schema.org',
             '@type': 'Recipe',
             name: recipe.name,
-            image: recipe.image,
+            image: recipe.image.url,
             datePublished: recipe.datePublished,
             description: recipe.description,
             keywords: recipe.keywords,
@@ -33,7 +40,11 @@ export default function RecipeTemplate({ recipe }) {
         </Link>
         <h1>{recipe.name}</h1>
         <p>{recipe.description}</p>
-        <img src={recipe.image[0]} alt={recipe.name} width={480} />
+        <img
+          src={recipe.image.url}
+          alt={recipe.image.alt}
+          width={480}
+        />
         <h2>Ingredients</h2>
         <ul>
           {recipe.ingredients.map((ingredient: string, idx: number) => (
@@ -57,9 +68,10 @@ export const getServerSideProps: GetServerSideProps = async () => {
       recipe: {
         slug: 'non-alcoholic-pina-colada',
         name: 'Non-alcoholic Pina Colada',
-        image: [
-          'https://img.cybercook.com.br/receitas/312/pina-colada-5.jpeg'
-        ],
+        image: {
+          url: 'https://img.cybercook.com.br/receitas/312/pina-colada-5.jpeg',
+          alt: 'Non-alcoholic Pina Colada'
+        },
         datePublished: '2018-03-10',
         description: 'This non-alcoholic pina colada is my favorite!',
         keywords: 'non-alcoholic',
@@ -72,7 +84,8 @@ export const getServerSideProps: GetServerSideProps = async () => {
           'Blend 2 cups of pineapple juice and 5/8 cup cream of coconut until smooth.',
           'Fill a glass with ice.',
           'Pour the pineapple juice and coconut mixture over ice.'
-        ]
+        ],
+        locale: 'en-US'
       }
     }
   }
